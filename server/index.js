87dfb8a3350a372d44c5386a77b064c793ba8901
Heitor <heitorsclaudino04@gmail.com>
2023-03-25 21:38:40 -0300

    First commits

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
+app.use(cors);
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    
+});
+
+server.listen(3001, () => console.log("Server running on port 3001."));
